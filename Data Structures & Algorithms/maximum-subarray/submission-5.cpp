8f86ff49c373bc858eeed6dff5e1986b365bc64e
aristi1215 <99367548+aristi1215@users.noreply.github.com>
2026-04-11 12:22:09 -0700

    Add: maximum-subarray - submission-5

---
+++ Data Structures & Algorithms/maximum-subarray/submission-5.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int maxSubArray(vector<int>& nums) {
+
+        int currSum = 0;
+        int maxSum = INT_MIN;
+
+        for(int i = 0; i < nums.size();i++) {
+            
+            if(currSum < 0) {
+                currSum = nums[i];
+            }else {
+                currSum += nums[i];
+            }
+
+             if(currSum >= maxSum){
+                maxSum = currSum;
+            }
+
+        }
+
+        return maxSum;
+
+    }
+};
